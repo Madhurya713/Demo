6e588edbaef1bd55a10d5820ca671693e5deda25
Madhurya Kumar <your_email@example.com>
2024-01-28 19:50:46 +0530

    Initial commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,21 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+@Slf4j
 public class DemoApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		log.info("Application is starting");
 	}
 
 }
